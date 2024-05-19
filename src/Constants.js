a9c52d5cb91055fa052773dd95817463954d3400
Deep13 <deep.deepak30@gmail.com>
2024-05-19 12:52:36 +0530

    initial

---
+++ src/Constants.js
@@ -0,0 +1,155 @@
+export const day_title = 'Happy Guru Gobind Singh Jayanti';
+export const banner_key1 = 'ca-app-pub-4717579333914549/1878594589';
+export const banner_key2 = 'ca-app-pub-4717579333914549/2666236107';
+export const banner_key3 = 'ca-app-pub-4717579333914549/8252431241';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/6413909426';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/1161582749';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/9367375829';
+export const language = ['English', 'Hindi', 'Punjabi'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'Heartiest wishes to you and your family on this auspicious occasion. May this Gurpurab bring lots of joy and happiness to your life. Happy Guru Govind Singh Ji Jayanti.',
+      },
+      {
+        id: '002',
+        text:
+          'May Guru Nanak Birthday enlighten your heart & mind with knowledge & sanctity.  Happy Guru Govind Singh Ji Jayanti!',
+      },
+      {
+        id: '003',
+        text:
+          "May your life full of golden days always with Guru's blessings warm wishes on  Happy Guru Govind Singh Ji's birthday.",
+      },
+      {
+        id: '004',
+        text:
+          'May happiness and blessings surround you as we join together to remember the beloved Sri Guru Govind Singh Ji. Happy Guru Govind Singh Ji Jayanti.',
+      },
+      {
+        id: '005',
+        text:
+          'May Guru Govind Singh Ji Give You, The Courage And Strength To Fight The Evil, And Always Stand By The Side Of Truth. Happy Guru Govind Singh Ji Jayanti.',
+      },
+      {
+        id: '006',
+        text:
+          'Heartiest wishes to you and your family on the auspicious occasion of Guru Govind Singh Ji’s birth anniversary. May this day bring lots of joy and happiness to your life. Happy Guru Govind Singh Ji Jayanti!',
+      },
+      {
+        id: '007',
+        text:
+          'May Guru Gobind Singh Ji give you the courage and strength to fight the evil and always stand by the side of truth. Wishing you a very happy Gurpurab! Happy Guru Govind Singh Ji Jayanti!',
+      },
+      {
+        id: '008',
+        text:
+          'May Guru Ji inspire you to achieve all your goals, dreams and ambitions. May His blessings be with you in all your endeavours! Happy Guru Govind Singh Ji Jayanti!!',
+      },
+      {
+        id: '009',
+        text:
+          'Waheguru Ji Da Khalsa....... Waheguru Ji Di Fateh....... A Happy Guru Gobind Singh Birthday..!!!',
+      },
+      {
+        id: '010',
+        text:
+          "On the auspicious occasion of Guru Govind Singh Ji Jayanti, I wish that you are showered with Guru Ji's divine blessings today and forever. Happy Guru Govind Singh Ji Jayanti.",
+      },
+    ]
+  }
+  ,
+  Hindi: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'गुरु गोविंद सिंह ने हमेशा यह उपदेश दिया कि भगवान तक पहुंचने के लिए प्रेम ही एक माध्यम है, तो चलिए उनके कहे शब्दों का अनुसरण करके उनका जन्मदिन मनाएं.. हैप्पी गुरु गोविंद सिंह जयंती..',
+      },
+      {
+        id: '002',
+        text:
+          'वाहे गुरु का आशीष सदा मिले, ऐसी है कामना मेरी!! गुरु की कृपा से आएगी, घर-घर में खुशहाली!! गुरु गोविंद सिंह जयंती की बधाइयां!!!.',
+      },
+      {
+        id: '003',
+        text:
+          'गुरु गोविन्द सिंह जी के सद्कर्महमे सदा दिखाएँगे राह,वाहे गुरु के ज्ञान सेसबके बिगड़े हुए काम बन जाएँगे...गुरु गोविन्द सिंह जयंती की हार्दिक शुभकामनाए..',
+      },
+      {
+        id: '004',
+        text:
+          'सवा लाख से एक लड़ाऊं, चिड़ियों सों मैं बाज तड़ऊँ, तभी गोविंद सिंह नाम कहांऊँ.. श्री गोविंद सिंह जी की जन्म जयंती पर उन्हें भावपूर्ण नमन्.',
+      },
+      {
+        id: '005',
+        text:
+          'सर पर मेरे हैं गुरुवर का हाथ,है हरपल हरदम वो मेरे साथ,है विश्वास वही राह दिखायेंगे,मेरे सारे बिगड़े काम बन जायेंगे,गुरु गोविन्द सिंह जयंती की हार्दिक शुभकामनाए..',
+      },
+      {
+        id: '006',
+        text:
+          'हो लाख-लाख बधाई आपको गुरु गोविन्द सिंह का आशीर्वाद मिले आपको ख़ुशी का जीवन से रिश्ता हो ऐसा दीये का बाती संग रिश्ता जैसा गुरु गोविन्द सिंह जयंती की हार्दिक शुभकामनाए..',
+      },
+      {
+        id: '007',
+        text:
+          'राज करेगा खालसा, बाके रहे ना कोए, वाहेगुरु जी का खालसा वाहे गुरु जी की फ़तेह..!! हैप्पी गुरु गोविन्द सिंह जयंती…!!!...',
+      },
+      {
+        id: '008',
+        text:
+          'सिखों के दसवें गुरु, महान वीर एक आदर्श व्यक्तित्व.. गुरु गोविंद सिंह जी की जयंती पर उन्हें श्रद्धापूर्वक नमन!..',
+      },
+    ]
+  },
+  Punjabi: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'ਗੁਰੂ ਗੋਵਿੰਦ ਸਿੰਘ ਨੇ ਹਮੇਸ਼ਾਂ ਉਪਦੇਸ਼ ਦਿੱਤਾ ਕਿ ਪ੍ਰੇਮ ਪ੍ਰਮਾਤਮਾ ਤੱਕ ਪਹੁੰਚਣ ਦਾ ਇਕ ਮਾਤਰ ਮਾਧਿਅਮ ਹੈ, ਇਸ ਲਈ ਆਓ ਉਸਦੇ ਜਨਮਦਿਨ ਨੂੰ ਉਨ੍ਹਾਂ ਦੇ ਸ਼ਬਦਾਂ ਦੀ ਪਾਲਣਾ ਕਰਦਿਆਂ ਮਨਾਓ .. ਧੰਨ ਗੁਰੂ ਗੋਵਿੰਦ ਸਿੰਘ ਜਯੰਤੀ ..',
+      },
+      {
+        id: '002',
+        text:
+          'ਗੁਰੂ ਦਾ ਅਸ਼ੀਰਵਾਦ ਸਦਾ ਰਹੇ, ਮੇਰੀ ਇਛਾ ਹੈ !! ਗੁਰੂ ਦੀ ਬਖਸ਼ਿਸ਼ ਨਾਲ ਆਉਣਗੇ, ਹਰ ਘਰ ਵਿੱਚ ਖੁਸ਼ੀ! ਗੁਰੂ ਗੋਬਿੰਦ ਸਿੰਘ ਜੈਅੰਤੀ ਦੀਆਂ ਮੁਬਾਰਕਾਂ !!!.',
+      },
+      {
+        id: '003',
+        text:
+          'ਗੁਰੂ ਗੋਬਿੰਦ ਸਿੰਘ ਜੀ ਦੇ ਨੇਕ ਕਾਰਜ ਹਮੇਸ਼ਾਂ ਰਸਤਾ ਵਿਖਾਉਣਗੇ, ਉਹ ਗੁਰੂ ਦੇ ਗਿਆਨ ਸਦਕਾ ਵਿਗਾੜੇ ਕੰਮ ਬਣ ਜਾਣਗੇ ... ਗੁਰੂ ਗੋਬਿੰਦ ਸਿੰਘ ਜੈਯੰਤੀ ਨੂੰ ਬਹੁਤ ਬਹੁਤ ਮੁਬਾਰਕਾਂ ..',
+      },
+      {
+        id: '004',
+        text:
+          "ਮੈਂ ਇਕ ਚੌਥਾਈ ਲੱਖ ਨਾਲ ਲੜਦਾ ਹਾਂ, ਮੈਨੂੰ ਚਿੜੀਆਂ ਨਾਲ ਬਾਜ ਕਰਨਾ ਚਾਹੀਦਾ ਹੈ, ਫਿਰ ਮੈਂ ਗੋਵਿੰਦ ਸਿੰਘ ਦੇ ਨਾਮ ਨਾਲ ਕਿੱਥੇ ਜਾਵਾਂ .. ਸ਼੍ਰੀ ਗੋਵਿੰਦ ਸਿੰਘ ਜੀ ਦੇ ਜਨਮ ਦਿਵਸ 'ਤੇ ਉਨ੍ਹਾਂ ਨੂੰ ਦਿਲੋਂ ਮੁਬਾਰਕਾਂ.",
+      },
+      {
+        id: '005',
+        text:
+          'ਮੇਰੇ ਸਿਰ ਤੇ ਗੁਰੂਵਰ ਦਾ ਹੱਥ ਹੈ, ਉਹ ਹਮੇਸ਼ਾਂ ਮੇਰੇ ਨਾਲ ਹੈ, ਮੈਨੂੰ ਵਿਸ਼ਵਾਸ ਹੈ ਕਿ ਮੈਂ ਉਹੀ ਰਸਤਾ ਵਿਖਾਵਾਂਗਾ, ਮੇਰੇ ਸਾਰੇ ਮਾੜੇ ਕੰਮ ਕੀਤੇ ਜਾਣਗੇ, ਗੁਰੂ ਗੋਬਿੰਦ ਸਿੰਘ ਜੈਯੰਤੀ ਦੀਆਂ ਦਿਲੋਂ ਇੱਛਾਵਾਂ ..',
+      },
+      {
+        id: '006',
+        text:
+          'ਵਧਾਈਆਂ, ਤੁਹਾਡਾ ਗੁਰੂ ਗੋਬਿੰਦ ਸਿੰਘ ਜੀ ਦਾ ਅਸ਼ੀਰਵਾਦ ਹੈ, ਤੁਹਾਡੇ ਜੀਵਨ ਨਾਲ ਇੱਕ ਖੁਸ਼ਹਾਲ ਰਿਸ਼ਤਾ ਹੈ, ਦੀਵੇ ਨਾਲ ਅਜਿਹਾ ਰਿਸ਼ਤਾ, ਗੁਰੂ ਗੋਬਿੰਦ ਸਿੰਘ ਜੈਯੰਤੀ ਦੀਆਂ ਦਿਲੋਂ ਇੱਛਾਵਾਂ ਵਰਗਾ ..',
+      },
+      {
+        id: '007',
+        text:
+          'ਰਾਜ ਕਰੇਗਾ ਖਾਲਸੇ, ਬੇਚੈਨ ਕੋਈ ਨਹੀਂ, ਵਾਹਿਗੁਰੂ ਜੀ ਦੇ ਖਾਲਸੇ ਵਾਲੇ ਗੁਰੂ ਜੀ ਦੀ ਫਤਿਹ .. !! ਧੰਨ ਗੁਰੂ ਗੋਵਿੰਦ ਸਿੰਘ ਜੈਅੰਤੀ… !!!…',
+      },
+      {
+        id: '008',
+        text:
+          'ਸਿੱਖਾਂ ਦੇ ਦਸਵੇਂ ਗੁਰੂ, ਮਹਾਨ ਵੀਰ ਇੱਕ ਆਦਰਸ਼ ਸ਼ਖਸੀਅਤ .. ਗੁਰੂ ਗੋਬਿੰਦ ਸਿੰਘ ਜੀ ਦੇ ਜਨਮ ਦਿਹਾੜੇ ਤੇ ਉਹਨਾਂ ਨੂੰ ਮੁਬਾਰਕਾਂ! ..',
+      },
+    ]
+  }
+
+
+};
